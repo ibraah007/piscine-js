928f64f804b51a4f90f11e85be8a3192179d70c7
ibrsamwel <ibraah568@gmail.com>
2026-05-05 15:25:48 +0300

    add nested object with arr and obj inside

--- declarations.js
+++ declarations.js
@@ -1,8 +1,17 @@
 const escapeStr =   "`\\/\"'";
 const arr = [4,'2'];
 const obj = {
-str:'anah',
+str:"ibrah",
 num:7,
 bool:false,
 undef:undefined,
+};
+const nested ={
+arr:[4 ,undefined '2'],
+obj:{
+str:"ibrah",
+num:6,
+bool:true
+}
+
 };
