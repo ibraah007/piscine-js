66bf78c474f2bf7f3b3e21c71ac4dc3ab646a5d8
ibrsamwel <ibraah568@gmail.com>
2026-05-06 11:46:42 +0300

    aded a test file given to test my code

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -11,3 +11,14 @@ function kiss (x) {
 return [last(x),first(x)];
 }
 
+// Test first function
+console.log(first([1, 2, 3]));  // 1
+console.log(first("hello"));     // "h"
+
+// Test last function
+console.log(last([1, 2, 3]));   // 3
+console.log(last("hello"));      // "o"
+
+// Test kiss function
+console.log(kiss([1, 2, 3]));   // [3, 1]
+console.log(kiss("hello"));      // ["o", "h"]
