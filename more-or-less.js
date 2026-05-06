a535d9b8e277e64240b8ac86567192d2b5983da0
ibrsamwel <ibraah568@gmail.com>
2026-05-06 05:07:17 +0300

    a test sample to test my functions

--- more-or-less.js
+++ more-or-less.js
@@ -14,3 +14,7 @@ function sub (x,y) {
 
 return x - y;
 }
+console.log(more(5)); // Should output 6
+console.log(less(5)); // Should output 4
+console.log(add(5, 3)); // Should output 8
+console.log(sub(5, 3)); // Should output 2
