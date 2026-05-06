633ed73ee18eb00b33cdc4a979afb15d84acff4e
ibrsamwel <ibraah568@gmail.com>
2026-05-06 11:04:48 +0300

    aded given test cases to verify my solution

--- returns.js
+++ returns.js
@@ -8,3 +8,16 @@ function getLength (x) {
 return x.length;
 
 }
+
+
+// Testing id function
+console.log(id(5)); // Should output 5
+console.log(id("hello")); // Should output 'hello'
+console.log(id([1, 2, 3])); // Should output [1, 2, 3]
+
+// Testing getLength function
+console.log(getLength("hello")); // Should output 5
+console.log(getLength([1, 2, 3])); // Should output 3
+console.log(getLength([])); // Should output 0
+console.log(getLength("")); // Should output 0
+
