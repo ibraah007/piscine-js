36f9259ce477e0064da43cb9e62019feaaa0b869
ibrsamwel <ibraah568@gmail.com>
2026-05-07 11:59:35 +0300

    created a function talkking two arguments and stored earths seconds and other planets orbital

---
+++ dog-years.js
@@ -0,0 +1,20 @@
+function dogYears (planet,seconds) {
+const earth seconds=31,557,600
+
+    const periods = {
+        earth: 1.0,
+        mercury: 0.2408467,
+        venus: 0.61519726,
+        mars: 1.8808158,
+        jupiter: 11.862615,
+        saturn: 29.447498,
+        uranus: 84.016846,
+        neptune: 164.79132
+    };
+
+
+
+
+
+
+}
