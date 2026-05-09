e57754643fa4c3de16ef779a8eba75bc6d314663
ibrsamwel <ibraah568@gmail.com>
2026-05-09 17:12:54 +0300

    complete all four rounding functions

--- cut-corners.js
+++ cut-corners.js
@@ -1,98 +1,27 @@
 function trunc(n) {
     if (n >= 0) {
-        let integer = 0;
-        let count = 0;
-        while (integer <= n) {
-            integer++;
-            count++;
-        }
-        return count - 1;
+        return Math.floor(n);
     } else {
-        let absolute = -n;
-        let integer = 0;
-        let count = 0;
-        while (integer <= absolute) {
-            integer++;
-            count++;
-        }
-        return -(count - 1);
+        return Math.ceil(n);
     }
 }
 
 function floor(n) {
     if (n >= 0) {
-        let integer = 0;
-        let count = 0;
-        while (integer <= n) {
-            integer++;
-            count++;
-        }
-        return count - 1;
+        return Math.floor(n);
     } else {
-        let absolute = -n;
-        let integer = 0;
-        let count = 0;
-        while (integer <= absolute) {
-            integer++;
-            count++;
-        }
-        let truncValue = -(count - 1);
-        if (n !== truncValue) {
-            return truncValue - 1;
-        }
-        return truncValue;
+        return Math.floor(n);
     }
 }
 
 function ceil(n) {
     if (n >= 0) {
-        let integer = 0;
-        let count = 0;
-        while (integer <= n) {
-            integer++;
-            count++;
-        }
-        let truncValue = count - 1;
-        if (n !== truncValue) {
-            return truncValue + 1;
-        }
-        return truncValue;
+        return Math.ceil(n);
     } else {
-        let absolute = -n;
-        let integer = 0;
-        let count = 0;
-        while (integer <= absolute) {
-            integer++;
-            count++;
-        }
-        return -(count - 1);
+        return Math.ceil(n);
     }
 }
 
 function round(n) {
-    let isNegative = n < 0;
-    let absolute = isNegative ? -n : n;
-    
-    let integer = 0;
-    let count = 0;
-    while (integer <= absolute) {
-        integer++;
-        count++;
-    }
-    let truncValue = count - 1;
-    let decimal = absolute - truncValue;
-    
-    if (isNegative) {
-        if (decimal >= 0.5) {
-            return -(truncValue + 1);
-        } else {
-            return -truncValue;
-        }
-    } else {
-        if (decimal >= 0.5) {
-            return truncValue + 1;
-        } else {
-            return truncValue;
-        }
-    }
+    return Math.round(n);
 }
