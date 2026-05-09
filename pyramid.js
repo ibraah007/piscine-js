233c69aa8b6e02b68419e87616f9b721959c011e
ibrsamwel <ibraah568@gmail.com>
2026-05-09 20:03:17 +0300

    final pyramid fix

--- pyramid.js
+++ pyramid.js
@@ -2,20 +2,11 @@ function pyramid(char, height) {
     let result = "";
     
     for (let i = 1; i <= height; i++) {
-        // Calculate number of spaces
         let spaces = height - i;
+        let stars = 2 * i - 1;
         
-        // Add spaces
-        for (let s = 0; s < spaces; s++) {
-            result = result + " ";
-        }
+        result = result + " ".repeat(spaces) + char.repeat(stars);
         
-        // Add characters (2 * i - 1)
-        for (let c = 0; c < (2 * i - 1); c++) {
-            result = result + char;
-        }
-        
-        // Add new line after each row except the last one
         if (i !== height) {
             result = result + "\n";
         }
