27b50efaf94207a3df82b2463531aff141f65e9b
ibrsamwel <ibraah568@gmail.com>
2026-05-09 20:06:06 +0300

    final pyramid fix

--- pyramid.js
+++ pyramid.js
@@ -1,12 +1,14 @@
 function pyramid(char, height) {
     let result = "";
-    
+    let charWidth = char.length; // Calculate how wide the string is
+
     for (let i = 1; i <= height; i++) {
-        let spaces = height - i;
+        // Multiply spaces by charWidth to maintain alignment
+        let spaces = (height - i) * charWidth; 
         let stars = 2 * i - 1;
-        
+
         result = result + " ".repeat(spaces) + char.repeat(stars);
-        
+
         if (i !== height) {
             result = result + "\n";
         }
