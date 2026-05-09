3fa35872f8b19da2baadea5305eef62817b9c1c2
ibrsamwel <ibraah568@gmail.com>
2026-05-09 19:51:28 +0300

    fix pyramid - use last row width to calculate correct spaces

--- pyramid.js
+++ pyramid.js
@@ -1,17 +1,23 @@
 function pyramid(char, height) {
     let result = "";
     
+    // Calculate width of the last row
+    let lastRowWidth = 2 * height - 1;
+    
     for (let i = 1; i <= height; i++) {
-        // Calculate number of spaces
-        let spaces = height - i;
+        // Number of stars in current row
+        let stars = 2 * i - 1;
+        
+        // Number of spaces to center
+        let spaces = (lastRowWidth - stars) / 2;
         
         // Add spaces
         for (let s = 0; s < spaces; s++) {
             result = result + " ";
         }
         
-        // Add characters (2 * i - 1)
-        for (let c = 0; c < (2 * i - 1); c++) {
+        // Add characters
+        for (let c = 0; c < stars; c++) {
             result = result + char;
         }
         
