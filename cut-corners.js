1c9185a64c862c3a9298c14066762c611ab07548
ibrsamwel <ibraah568@gmail.com>
2026-05-09 16:52:53 +0300

    add floor function - rounds down to nearest integer

--- cut-corners.js
+++ cut-corners.js
@@ -21,3 +21,33 @@ function trunc(n) {
         return -(count - 1);
     }
 }
+
+
+function floor(n) {
+    
+    if (n >= 0) {
+        let integer = 0;
+        let count = 0;
+        while (integer <= n) {
+            integer++;
+            count++;
+        }
+        return count - 1;
+    } else {
+        
+        let absolute = -n;
+        let integer = 0;
+        let count = 0;
+        while (integer <= absolute) {
+            integer++;
+            count++;
+        }
+        let truncValue = -(count - 1);
+        
+        
+        if (n !== truncValue) {
+            return truncValue - 1;
+        }
+        return truncValue;
+    }
+}
