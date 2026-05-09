b2f92df618f656a65c6cadd0fd31cac5ab75b3d9
ibrsamwel <ibraah568@gmail.com>
2026-05-09 16:55:35 +0300

    add ceil function - rounds up to nearest integer

--- cut-corners.js
+++ cut-corners.js
@@ -51,3 +51,31 @@ function floor(n) {
         return truncValue;
     }
 }
+function ceil(n) {
+    
+    if (n >= 0) {
+        let integer = 0;
+        let count = 0;
+        while (integer <= n) {
+            integer++;
+            count++;
+        }
+        let truncValue = count - 1;
+        
+        
+        if (n !== truncValue) {
+            return truncValue + 1;
+        }
+        return truncValue;
+    } else {
+        
+        let absolute = -n;
+        let integer = 0;
+        let count = 0;
+        while (integer <= absolute) {
+            integer++;
+            count++;
+        }
+        return -(count - 1);
+    }
+}
