8b71b9fbc6fdeaa8323132453161269cf557c698
ibrsamwel <ibraah568@gmail.com>
2026-05-09 19:12:07 +0300

    remove old chunk.js file

--- chunk.js
+++
@@ -1,9 +0,0 @@
-function chunk(arr, size) {
-    let result = [];
-    
-    for (let i = 0; i < arr.length; i += size) {
-        result.push(arr.slice(i, i + size));
-    }
-    
-    return result;
-}
