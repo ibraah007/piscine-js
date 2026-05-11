38d6c4d930931d019bdd83d0b7083715be12e039
ibrsamwel <ibraah568@gmail.com>
2026-05-11 14:11:28 +0300

    upgraded it to have a global identifier

--- same-amount.js
+++ same-amount.js
@@ -1,5 +1,12 @@
 function sameAmount(str, reg1, reg2) {
 
+ let regex1 = new RegExp(reg1.source, 'g');
+    let regex2 = new RegExp(reg2.source, 'g');
+    
+
+
+
+
 let matches1= str.match(reg1)
 
 let count1=matches ? matches1.length : 0
