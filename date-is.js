a508dd26439890e4d53a9dac6d46215dcbfd9f17
ibrsamwel <ibraah568@gmail.com>
2026-05-12 12:26:23 +0300

    add isAfter function - compares if first date is after second date

--- date-is.js
+++ date-is.js
@@ -14,3 +14,13 @@ function isValid(date) {
     
     return false;
 }
+function isAfter(date1, date2) {
+   
+    const time1 = date1 instanceof Date ? date1.getTime() : date1;
+    
+    
+    const time2 = date2 instanceof Date ? date2.getTime() : date2;
+    
+    
+    return time1 > time2;
+}
