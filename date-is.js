e005bb3405202bc338990e5d940bd566c49efab1
ibrsamwel <ibraah568@gmail.com>
2026-05-12 12:34:46 +0300

    fix date-is - ensure all functions are properly defined

--- date-is.js
+++ date-is.js
@@ -1,52 +1,35 @@
 function isValid(date) {
-    
     if (typeof date === 'number') {
-      
         return !isNaN(date);
     }
-    
-    
     if (date instanceof Date) {
-        
         return !isNaN(date.getTime());
     }
-    
-    
     return false;
 }
+
 function isAfter(date1, date2) {
-   
     const time1 = date1 instanceof Date ? date1.getTime() : date1;
-    
-    
     const time2 = date2 instanceof Date ? date2.getTime() : date2;
-    
-    
     return time1 > time2;
 }
+
+function isBefore(date1, date2) {
+    const time1 = date1 instanceof Date ? date1.getTime() : date1;
+    const time2 = date2 instanceof Date ? date2.getTime() : date2;
+    return time1 < time2;
+}
+
 function isFuture(date) {
-    
     if (!isValid(date)) return false;
-    
-    
     const now = Date.now();
-    
-    
     const time = date instanceof Date ? date.getTime() : date;
-    
-    
     return time > now;
 }
+
 function isPast(date) {
-    
     if (!isValid(date)) return false;
-    
-    
     const now = Date.now();
-    
-    
     const time = date instanceof Date ? date.getTime() : date;
-    
-    
     return time < now;
 }
