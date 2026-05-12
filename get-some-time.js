75368c3e4c13dd3ee1ba5f07e290a25fd30a7924
ibrsamwel <ibraah568@gmail.com>
2026-05-12 14:52:06 +0300

    done the reqired task

--- get-some-time.js
+++ get-some-time.js
@@ -1,10 +1,30 @@
- let targetMonday = new Date(firstMonday);
-    targetMonday.setDate(firstMonday.getDate() + (week - 1) * 7);function firstDayWeek(week, year) {
-
+function firstDayWeek(week, year) {
+    
     let date = new Date(year + "-01-01");
+    
+    
     let dayOfWeek = date.getDay();
+    
+   
     let daysToMonday = (dayOfWeek === 0 ? 6 : dayOfWeek - 1);
+    
+    
     let firstMonday = new Date(date);
     firstMonday.setDate(date.getDate() - daysToMonday);
+    
+    
+    let targetMonday = new Date(firstMonday);
+    targetMonday.setDate(firstMonday.getDate() + (week - 1) * 7);
+    
+    
+    if (targetMonday.getFullYear() < parseInt(year)) {
+        targetMonday = new Date(year + "-01-01");
+    }
+    
+    
+    let day = String(targetMonday.getDate()).padStart(2, '0');
+    let month = String(targetMonday.getMonth() + 1).padStart(2, '0');
+    let targetYear = targetMonday.getFullYear();
+    
     
 }
