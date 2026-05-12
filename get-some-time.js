9baf6e8b7f826b57eb77ef6dc6246da07a3bcae9
ibrsamwel <ibraah568@gmail.com>
2026-05-12 14:44:27 +0300

    done first monday

--- get-some-time.js
+++ get-some-time.js
@@ -1,3 +1,9 @@
 function firstDayWeek(week, year) {
+
+    let date = new Date(year + "-01-01");
+    let dayOfWeek = date.getDay();
+    let daysToMonday = (dayOfWeek === 0 ? 6 : dayOfWeek - 1);
+    let firstMonday = new Date(date);
+    firstMonday.setDate(date.getDate() - daysToMonday);
     
 }
