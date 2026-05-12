81104005c8defd3da27b67c74fff7daa27b8e771
ibrsamwel <ibraah568@gmail.com>
2026-05-12 12:00:25 +0300

    modified a feature that gets time stamp for dates

--- date-is.js
+++ date-is.js
@@ -4,12 +4,12 @@ return  dateinstanceof Date && !isNan(date.getTime())
 }
 function isAfter(date1, date2) {
 
-return date1>date2
+return date1.getTime()>date2.getTime()
 
 
 }
 function isBefore(date1, date2) {
-    return date1. < date2.
+    return date1.getTime() < date2.getTime()
 }
 function isFuture(date){
 if (!isValid(date)) return false
