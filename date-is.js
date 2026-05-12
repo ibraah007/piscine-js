ef824d12b0615fa088a5152d3cf61e881a88d29b
ibrsamwel <ibraah568@gmail.com>
2026-05-12 12:18:34 +0300

    add isValid handles Date objects and timestamps

--- date-is.js
+++ date-is.js
@@ -1,27 +1,16 @@
- function isValid(date){
-return  date instanceof Date && !isNaN(date.getTime())
-
-}
-function isAfter(date1, date2) {
-
-return date1.getTime()>date2.getTime()
-
-
-}
-function isBefore(date1, date2) {
-    return date1.getTime() < date2.getTime()
-}
-function isFuture(date){
-if (!isValid(date)) return false
-const now = new Date()
-return date.getTime() > now.getTime()
-
-}
-function isPast(date) {
-if (!isValid(date)) return false
-const now = new Date()
-return date.getTime() < now.getTime()
-
-
-
+function isValid(date) {
+    
+    if (typeof date === 'number') {
+      
+        return !isNaN(date);
+    }
+    
+    
+    if (date instanceof Date) {
+        
+        return !isNaN(date.getTime());
+    }
+    
+    
+    return false;
 }
